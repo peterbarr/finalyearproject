a18e14de62f8b0cd94d20db0a7e3097bcc67c0d6
Peter Barr <50499395+peterbarr@users.noreply.github.com>
2021-04-28 18:32:21 +0100

    Update request.py

--- IntrusionDetection/request.py
+++ IntrusionDetection/request.py
@@ -10,10 +10,10 @@ url = 'http://localhost:5000/results'
 
 r = requests.post(url,json={"src_bytes":491,
                             "dst_bytes":0,
-             			    "same_srv_rate":1.0,
-             			    "diff_srv_rate":0.0,
-             			    "dst_host_same_srv_rate":0.17,
-             			    "flag":1
+             			          "same_srv_rate":1.0,
+             			          "diff_srv_rate":0.0,
+             			          "dst_host_same_srv_rate":0.17,
+                            "flag":1
                             })
 
 print(r.json())
